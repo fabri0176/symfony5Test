3aedb338cafb526a9fae0659137fd58ca3239149
Nestor F Parra <nparra@existaya.com>
2020-06-07 13:04:22 -0500

    Ignorar carpeta upload y agregar carga de fichero

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -25,6 +25,31 @@ class PostsController extends AbstractController {
             //Cargar usuario logueado
             $user = $this->getUser();
 
+            $foto = $form->get('foto')->getData();
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($foto) {
+                $originalFilename = pathinfo($foto->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()',$originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$foto->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $foto->move(
+                        $this->getParameter('foto_post_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                    throw new \Exception("Se ha generado un error");
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $post->setFoto($newFilename);
+            }
+
             $post->setUser($user);
 
             //Guardamos en la base de datos
--- src/Form/PostsType.php
+++ src/Form/PostsType.php
@@ -4,17 +4,23 @@ namespace App\Form;
 
 use App\Entity\Posts;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use function Sodium\add;
 
 class PostsType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
                 ->add('titulo')
-                ->add('foto')
+                ->add('foto',FileType::class,[
+                    'label' => 'Seleccione una imagen',
+                    'mapped' => false,
+                    'required' => false,
+                    ])
                 ->add('contenido',TextareaType::class, [
                     'attr' => ['class' => 'tinymce'],
                 ])
